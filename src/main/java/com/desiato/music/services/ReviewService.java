8ea8592935e73e650197b613f0b85ce144f8931a
Giuseppe Desiato <giodesiato@gmail.com>
2023-06-17 12:57:38 -0400

    review controller

---
+++ src/main/java/com/desiato/music/services/ReviewService.java
@@ -0,0 +1,36 @@
+package com.desiato.music.services;
+
+import com.desiato.music.models.Album;
+import com.desiato.music.models.Review;
+import com.desiato.music.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+
+    public Review createReview(String reviewBody, String musicBrainzId){
+
+        // create and persist a new review
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        // associare the review with one of the movies
+        mongoTemplate.update(Album.class)
+                // updating the movie where the musicBrainzId matches the musicBrainz Id received from the user
+                .matching(Criteria.where("musicBrainzId").is(musicBrainzId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
