873927358fea64839221e9c5c07f6369bd68c6f6
Giuseppe Desiato <giodesiato@gmail.com>
2023-07-03 16:49:46 -0400

    review mthods to ReviewController

--- src/main/java/com/desiato/music/controllers/AlbumController.java
+++ src/main/java/com/desiato/music/controllers/AlbumController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Objects;
@@ -78,41 +77,4 @@ public class AlbumController {
         return ResponseEntity.ok(album);
     }
 
-    @GetMapping("/{id}/review")
-    public String getReviewForm(@PathVariable("id") String id, Model model) {
-        Optional<Album> optionalAlbum = albumService.albumById(id);
-        if(optionalAlbum.isPresent()) {
-            model.addAttribute("album", optionalAlbum.get());
-            return "review"; // Show review form
-        } else {
-            model.addAttribute("error", "The album you're looking for doesn't exist.");
-            return "error";  // Show error page
-        }
-    }
-
-    @PostMapping("/{id}/review")
-    public String submitReview(@PathVariable("id") String id,
-                               @RequestParam("review") String reviewContent,
-                               Model model,
-                               RedirectAttributes redirectAttributes) {
-        Optional<Album> optionalAlbum = albumService.albumById(id);
-        if (optionalAlbum.isEmpty()) {
-            model.addAttribute("error", "The album you're looking for doesn't exist.");
-            return "error"; // Show error page
-        }
-
-        Album album = optionalAlbum.get();
-        Review review = new Review(); // Create a new Review object
-        review.setComment(reviewContent); // Set the review content
-        review = reviewService.save(review); // Save the review
-        album.getReviewIds().add(review); // Add the review to the album
-
-        // Update the album's overall rating
-        albumService.calculateNewRating(album);
-
-        albumService.save(album); // Save the album
-
-        redirectAttributes.addFlashAttribute("message", "Your review was successfully submitted.");
-        return "redirect:/albums/" + id; // Redirect to the album detail page
-    }
 }
--- src/main/java/com/desiato/music/controllers/ReviewController.java
+++ src/main/java/com/desiato/music/controllers/ReviewController.java
@@ -1,26 +1,59 @@
 package com.desiato.music.controllers;
 
+import com.desiato.music.models.Album;
 import com.desiato.music.models.Review;
+import com.desiato.music.services.AlbumService;
 import com.desiato.music.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Map;
+import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/v1/reviews")
+@Controller
+@RequestMapping("reviews")
 public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
 
-    @PostMapping
-    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<Review>(reviewService.createReview(payload.get("name"), payload.get("reviewBody"), payload.get("musicBrainzId")), HttpStatus.CREATED);
+    @Autowired
+    private AlbumService albumService;
+
+    @GetMapping("/create/{albumId}")
+    public String getReviewForm(@PathVariable("albumId") String albumId, Model model) {
+        Optional<Album> optionalAlbum = albumService.albumById(albumId);
+        if(optionalAlbum.isPresent()) {
+            model.addAttribute("album", optionalAlbum.get());
+            return "review"; // Show review form
+        } else {
+            model.addAttribute("error", "The album you're looking for doesn't exist.");
+            return "error";  // Show error page
+        }
+    }
+
+    @PostMapping("/create/{albumId}")
+    public String submitReview(@PathVariable("albumId") String albumId,
+                               @ModelAttribute("review") Review review,
+                               RedirectAttributes redirectAttributes) {
+        Optional<Album> optionalAlbum = albumService.albumById(albumId);
+        if (optionalAlbum.isEmpty()) {
+            redirectAttributes.addFlashAttribute("error", "The album you're looking for doesn't exist.");
+            return "redirect:/reviews/create/"+albumId; //redirect back to the creation form
+        }
+
+        Album album = optionalAlbum.get();
+        review = reviewService.save(review); // Save the review
+        album.getReviewIds().add(review); // Add the review to the album
+
+        // Update the album's overall rating
+        albumService.calculateNewRating(album);
+
+        albumService.save(album); // Save the album
+
+        redirectAttributes.addFlashAttribute("message", "Your review was successfully submitted.");
+        return "redirect:/albums/" + albumId; // Redirect to the album detail page
     }
 }
