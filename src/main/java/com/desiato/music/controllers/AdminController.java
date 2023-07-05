67e7f69a7e8ad55c289bdf6eeed3d2fbeef9aa40
Giuseppe Desiato <giodesiato@gmail.com>
2023-07-05 10:27:03 -0400

    user list roles

--- src/main/java/com/desiato/music/controllers/AdminController.java
+++ src/main/java/com/desiato/music/controllers/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/admin")
+//@RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
